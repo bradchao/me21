a580199ffb7bfa4955200a4f40e4acfa0505bcc9
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-14 16:30:57 +0800

    2112141630

--- brad14.php
+++ brad14.php
@@ -1,4 +1,28 @@
+<?php
+    $isLeap = '';
+    if (isset($_GET['year'])){
+        $year = $_GET['year'];
+        if ($year % 4 == 0){
+            if ($year % 100 == 0){
+                if ($year % 400 == 0){
+                    // 潤
+                    $isLeap = true;
+                }else{
+                    // 平
+                    $isLeap = false;
+                }
+            }else{
+                // 潤
+                $isLeap = true;
+            }
+        }else{
+            // 平
+            $isLeap = false;
+        }
+    }
+?>
 <form>
 年:<input type="number" name="year" />
 <input type="submit" value="判斷" />
+<?php echo $isLeap?'潤':'平'; ?>
 </form>
