6ad3b46951cce9a04125c249df6f8a0460b6a392
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-14 15:41:38 +0800

    2112141541

--- brad10.php
+++ brad10.php
@@ -1,8 +1,25 @@
 <?php
-    $result = $x = $y = '';
-    if (isset($_GET['x']) && $_GET['y']){
+    $result = $x = $y = $op = '';
+    if (isset($_GET['x']) && isset($_GET['y'])){
         $x = $_GET['x']; $y = $_GET['y'];
-        $result = $x + $y;
+        $op = $_GET['op'];
+
+        switch ($op){
+            case '1':
+                $result = $x + $y;
+                break;        
+            case '2':
+                $result = $x - $y;
+                break;        
+            case '3':
+                $result = $x * $y;
+                break;        
+            case '4':
+                $result = $x / $y;
+                break;        
+        }
+
+        // $result = $x + $y;
         // echo "{$x} + {$y} = {$result}";        
     } 
 
@@ -10,10 +27,10 @@
 <form>
     <input name='x' value="<?php echo $x; ?>" />
     <select name='op'>
-        <option value='1'>+</option>
-        <option value='2'>-</option>
-        <option value='3'>x</option>
-        <option value='4'>/</option>
+        <option value='1' <?php echo $op=='1'?'selected':''; ?>>+</option>
+        <option value='2' <?php echo $op=='2'?'selected':''; ?>>-</option>
+        <option value='3' <?php echo $op=='3'?'selected':''; ?>>x</option>
+        <option value='4' <?php echo $op=='4'?'selected':''; ?>>/</option>
     </select>
     <input name="y" value="<?php echo $y; ?>" />
     <input type="submit" value="=" />
---
+++ brad12.php
@@ -0,0 +1,10 @@
+<script>
+    //var a = 10;
+    document.write(a);
+    console.log(a);
+</script>
+<?php
+    //$a = 123;
+    echo $a;
+?>
+Notice -> Warning -> Error
