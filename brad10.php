f0a4cd838e1c0e4af4be6f3162970b74de97e09a
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-14 15:01:30 +0800

    2112141501

--- brad10.php
+++ brad10.php
@@ -5,10 +5,16 @@
         $result = $x + $y;
         // echo "{$x} + {$y} = {$result}";        
     } 
+
 ?>
 <form>
     <input name='x' value="<?php echo $x; ?>" />
-    +
+    <select name='op'>
+        <option value='1'>+</option>
+        <option value='2'>-</option>
+        <option value='3'>x</option>
+        <option value='4'>/</option>
+    </select>
     <input name="y" value="<?php echo $y; ?>" />
     <input type="submit" value="=" />
     <?php 
