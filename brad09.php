9145a7aef977fcb214f43b146dc57f7098098bf9
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-14 14:18:44 +0800

    2112141418

---
+++ brad09.php
@@ -0,0 +1,11 @@
+<?php
+    $a = 'Brad1'; $b = 'Chao2';
+    $c = $a . $b;
+    echo "$a $b";
+    echo '<hr />';
+    echo '$a $b\n';
+    echo '<hr />';
+    $w = 80.1;
+    echo "weight: {$w}kg\n";
+    echo "weight: {$w}kg";
+?>
---
+++ brad11.php
@@ -0,0 +1,6 @@
+<h1>I am brad11.php, haha</h1>
+<?php
+    $x = $_GET['x']; $y = $_GET['y'];
+    $result = $x + $y;
+    echo "{$x} + {$y} = {$result}";
+?>
