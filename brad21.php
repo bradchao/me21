9ac4723f0e4bacc7dc447c7678416d3a5529b0be
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-15 12:00:25 +0800

    2112151200

--- brad21.php
+++ brad21.php
@@ -1,46 +1,18 @@
 <table border="1" width="100%">
-    <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>
-    <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>
+    <?php
+        for ($k = 0; $k < 5; $k++){
+            echo '<tr>';
+            for ($j = 2; $j <= 5; $j++){
+                $newj = $j  + $k * 4;
+                echo '<td>'; 
+                for ($i = 1; $i <= 9; $i++){
+                    $r = $newj * $i;
+                    echo "{$newj} x {$i} = {$r}<br />";
+                }
+                echo '</td>';
+            }
+            echo '</tr>';    
+        }
+    ?>
+
 </table>
