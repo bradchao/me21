3838e17662dcf33c0f70e3917fa26c91cc79accd
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-15 14:17:47 +0800

    2112151417

---
+++ brad23.php
@@ -0,0 +1,18 @@
+<?php
+    $sum = $n = '';
+    if (isset($_GET['n'])){
+        $n = $_GET['n'];
+        $i = 1;
+        $sum = 0;
+        while ($i<=$n){
+            $sum += $i;
+            $i++;
+        }
+    }
+?>
+<form>
+    1 + 2 + ...... + 
+    <input type="number" name="n" value="<?php echo $n; ?>" />
+    <input type="submit" value="=" />
+    <span><?php echo $sum; ?></span>
+</form>
