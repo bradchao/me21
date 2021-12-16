6944c039b58721763c8c166a5d09fcc3a194bc59
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-16 12:03:33 +0800

    2112161203

--- pokerv3.php
+++ pokerv3.php
@@ -17,13 +17,20 @@
 ?>
 <table border="1" width="100%">
     <?php
+        $suits = ["&spades;","&hearts;","&diams;","&clubs;"];
+        $value = ['A', 2,3,4,5,6,7,8,9,10,'J','Q','K'];
         foreach($players as $player){
+            sort($player);
             echo '<tr>';
             foreach($player as $card){
-                echo "<td>{$card}</td>";
+                echo "<td>";
+                echo $suits[(int)($card / 13)];
+                echo $value[$card % 13];
+                echo "</td>";
             }
             echo '</tr>';    
         }
-
     ?>
 </table>
+
+
