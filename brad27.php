e0551d9a818fe5f348a8579a3922585c28513581
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-12-16 09:46:57 +0800

    2112160946

--- brad27.php
+++ brad27.php
@@ -2,11 +2,18 @@
     $p = array(1 => 0, 0, 0, 0, 0, 0);
 
     for($i = 0; $i < 100000 ; $i++){
-        $point = rand(1,6);
-        $p[$point]++;
+        $point = rand(1,9);
+        $p[ $point>6? $point-6 : $point ]++;
     }
 
     for ($i = 1; $i <= 6; $i++){
         echo "{$i}點出現{$p[$i]}次<br />";
     }
+
+    echo '<br />';
+
+    foreach($p as $k => $v){
+        echo "{$k}點出現{$v}次<br />";
+    }
+
 ?>
