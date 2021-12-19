ece6177b8bd8db585f70f4eeb39f52e7998b16f6
BradChao <brad@brad.tw>
2021-12-19 10:39:51 +0800

    2112200000

--- brad34.php
+++ brad34.php
@@ -2,9 +2,9 @@
     // 正規表示法 Regex
     $id = 'X133456789';
     if (preg_match('/^[A-Z][12][0-9]{8}$/', $id)){
-        echo 'OK';
+        echo 'OK1';
     }else{
-        echo 'XX';
+        echo 'XX2';
     }
 
 ?>
