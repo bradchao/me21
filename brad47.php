2504313d36a9428f4f6e8811c7501845234c2b55
BradChao <brad@brad.tw>
2021-12-21 10:25:05 +0800

    2112211025

--- brad47.php
+++ brad47.php
@@ -3,10 +3,12 @@
 
     $yellow = imagecolorallocate($img, 255, 255, 0);
 
-    imagettftext( $img, 48, 0, 100, 100, $yellow, './dir1/font1.ttf', 'Hello, World');
+    imagettftext( $img, 48, 0, 200, 400, $yellow, './dir1/font1.ttf', 'Hello, World');
 
     header('Content-type: image/jpeg');
     imagejpeg($img);
+
+    imagejpeg($img, './dir1/mycoffee.jpg');
 
     imagedestroy($img);
 
