56bf7a7d4d5aaa81c37db37e32b5ec7d8fe1940c
BradChao <brad@brad.tw>
2021-12-21 09:37:29 +0800

    2112210937

---
+++ brad45.php
@@ -0,0 +1,20 @@
+<?php
+
+    // 1. 建立畫布(空白/exist)
+    $img = ImageCreate(400, 24);
+    //var_dump($img);
+
+    // 2. 開始作畫
+    $red = imagecolorallocate($img, 255, 0, 0);
+    $yellow = imagecolorallocate($img, 255, 255, 0);
+    imagefill($img, 0, 0, $red);
+    imagefilledrectangle($img,0,0,200,24, $yellow);
+
+    // 3. 輸出(網頁/檔案)
+    header('Content-type: image/jpeg');
+    imagejpeg($img);
+
+    // 4. 釋放記憶體
+    ImageDestroy($img);
+
+?>
