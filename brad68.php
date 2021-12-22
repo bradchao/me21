aefc61e51632f9ebd1a34a40ae948f3c9f1adc86
BradChao <brad@brad.tw>
2021-12-22 15:29:49 +0800

    2112221529

--- brad68.php
+++ brad68.php
@@ -1,12 +1,13 @@
 <?php
-    if (!isset($_POST['account'])) header('Location: brad62.php');
+    if (!isset($_REQUEST['account'])) header('Location: brad62.php');
 
     include 'bradutils.php';
     $mysqli = connectSQL('iii');
 
-    $account = $_POST['account']; $passwd = $_POST['passwd'];
+    $account = $_REQUEST['account']; $passwd = $_REQUEST['passwd'];
 
     $sql = "SELECT id,account,passwd,realname FROM member WHERE account = ?";
+    $stmt = $mysqli->prepare($sql);
     $stmt->bind_param('s', $account);
     $stmt->execute();
     $stmt->store_result();
@@ -14,12 +15,18 @@
     if ($stmt->num_rows > 0){
         $stmt->bind_result($id, $account, $passwdHash, $realname);
         $stmt->fetch();
+
+        //echo $passwd . ":" . $passwdHash;
+
         if (password_verify($passwd, $passwdHash)){
             // OK
+            header('Location: brad69.php');
         }else{
             // XX
+            header('Location: brad62.php');
         }
     }else{
         // account not exist
+        header('Location: brad62.php');
     }
 ?>
